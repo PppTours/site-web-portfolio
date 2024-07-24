742863e006b48352c481ecbc9a8a34f22d2203d4
Jules Pascot <jules.pascot@gmail.com>
2024-07-24 08:33:34 +0200

    feat(student): adding seed

---
+++ packages/api/src/database/seeds/data/student.data.ts
@@ -0,0 +1,134 @@
+import { StudentCreationDTO } from 'src/student/dto/student-creation.dto';
+
+export const STUDENTS: StudentCreationDTO[] = [
+  {
+    firstName: 'John',
+    lastName: 'Doe',
+    profilePictureUrl:
+      'https://thumbs.wbm.im/pw/medium/55f909f894923fc2807bc6d88403e840.avif',
+    level: {
+      id: 1,
+    },
+    specialty: {
+      id: 4,
+    },
+  },
+  {
+    firstName: 'Alice',
+    lastName: 'Smith',
+    profilePictureUrl:
+      'https://i.pinimg.com/474x/b9/3d/d4/b93dd44ef0d0719d741a9c36802e6d82.jpg',
+    level: {
+      id: 2,
+    },
+    specialty: {
+      id: 6,
+    },
+  },
+  {
+    firstName: 'Emma',
+    lastName: 'Johnson',
+    profilePictureUrl: null,
+    level: {
+      id: 3,
+    },
+    specialty: {
+      id: 2,
+    },
+  },
+  {
+    firstName: 'Michael',
+    lastName: 'Williams',
+    profilePictureUrl:
+      'https://thumbs.wbm.im/pw/medium/39573f81d4d58261e5e1ed8f1ff890f6.avif',
+    level: {
+      id: 4,
+    },
+    specialty: {
+      id: 3,
+    },
+  },
+  {
+    firstName: 'James',
+    lastName: 'Brown',
+    profilePictureUrl:
+      'https://thumbs.wbm.im/pw/medium/6a9b235faaa86202932278ed19bb76bf.avif',
+    level: {
+      id: 5,
+    },
+    specialty: {
+      id: 5,
+    },
+  },
+  {
+    firstName: 'Sophia',
+    lastName: 'Miller',
+    profilePictureUrl: 'https://wrong.url.com/picture.png',
+    level: {
+      id: 5,
+    },
+    specialty: {
+      id: 7,
+    },
+  },
+  {
+    firstName: 'Olivia',
+    lastName: 'Davis',
+    profilePictureUrl:
+      'https://i.pinimg.com/474x/e9/c8/08/e9c808fdac222166d8b1d892e79fe25e.jpg',
+    level: {
+      id: 6,
+    },
+    specialty: {
+      id: 4,
+    },
+  },
+  {
+    firstName: 'William',
+    lastName: 'Wilson',
+    profilePictureUrl:
+      'https://images.unsplash.com/photo-1539571696357-5a69c17a67c6?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    level: {
+      id: 3,
+    },
+    specialty: {
+      id: 3,
+    },
+  },
+  {
+    firstName: 'Benjamin',
+    lastName: 'Moore',
+    profilePictureUrl:
+      'https://images.unsplash.com/photo-1499996860823-5214fcc65f8f?q=80&w=1366&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    level: {
+      id: 5,
+    },
+    specialty: {
+      id: 2,
+    },
+  },
+  {
+    firstName: 'Emily',
+    lastName: 'Taylor',
+    profilePictureUrl:
+      'https://i.pinimg.com/564x/c6/dc/2e/c6dc2ea586c471521004b3eb12862472.jpg',
+    level: {
+      id: 5,
+    },
+    specialty: {
+      id: 3,
+    },
+  },
+  {
+    firstName: 'Jane',
+    lastName: 'Floyd',
+    profilePictureUrl:
+      'https://images.unsplash.com/photo-1529626455594-4ff0802cfb7e?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    level: {
+      id: 1,
+    },
+    specialty: {
+      id: 4,
+    },
+  },
+];
--- packages/api/src/database/seeds/seed.service.ts
+++ packages/api/src/database/seeds/seed.service.ts
@@ -3,20 +3,25 @@ import { StudyLevelEntity } from 'src/study-level/study-level.entity';
 import { StudySpecialtyEntity } from 'src/study-specialty/study-specialty.entity';
 import { STUDY_LEVELS } from './data/study-level.data';
 import { STUDY_SPECIALTIES } from './data/study-specialty.data';
+import { STUDENTS } from './data/student.data';
+import { StudentEntity } from 'src/student/student.entity';
 
 export class SeedService {
   private studyLevelRepository: Repository<StudyLevelEntity>;
   private studySpecialtyRepository: Repository<StudySpecialtyEntity>;
+  private studentRepository: Repository<StudentEntity>;
 
   constructor(private dataSource: DataSource) {
     this.studyLevelRepository = this.dataSource.getRepository(StudyLevelEntity);
     this.studySpecialtyRepository =
       this.dataSource.getRepository(StudySpecialtyEntity);
+    this.studentRepository = this.dataSource.getRepository(StudentEntity);
   }
 
   public async seedData(): Promise<void> {
     await this.seedStudyLevels();
     await this.seedStudySpecialties();
+    await this.seedStudents();
   }
 
   private async seedStudyLevels(): Promise<void> {
@@ -31,6 +36,13 @@ export class SeedService {
       const studySpecialtyEntity =
         this.studySpecialtyRepository.create(studySpecialty);
       await this.studySpecialtyRepository.save(studySpecialtyEntity);
+    }
+  }
+
+  private async seedStudents(): Promise<void> {
+    for (const student of STUDENTS) {
+      const studentEntity = this.studentRepository.create(student);
+      await this.studentRepository.save(studentEntity);
     }
   }
 }
